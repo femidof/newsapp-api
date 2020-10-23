c4b9e715dda7b135b5ed2ba80479754ae6596e97
Durosinmi Olorunfemi <dee_femifose@yahoo.com>
2020-10-22 20:31:20 -0500

    setting up authentication and verification dont work

---
+++ .history/app/User_20201022093606.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name', 'last_name', 'email', 'password', 'email_verified_at',
+        'api_token', 'user_type', 'liked_posts',
+        'disliked_posts', 'favorite_posts', 'favorite_categories', 'preferences',
+        'avatar',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
